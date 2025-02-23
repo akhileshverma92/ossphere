21e54e91baa567d74510fbc403e29ba71f6b1990
akhilesh_verma <146834811+akhileshverma92@users.noreply.github.com>
2025-02-23 10:07:02 +0530

    Update Appwrite.js
    Added appwrite for batabase and Oauth with appwrite

--- Osshpere_/src/Appwrite.js
+++ Osshpere_/src/Appwrite.js
@@ -12,9 +12,15 @@ client
     .setEndpoint(ENDPOINT)
     .setProject(PROJECT_ID);
 
+
+
 // Initialize Appwrite services
 export const account = new Account(client);
 export const databases = new Databases(client);
 
+
+databases.listDocuments('67b9f9980014a4b99691', '67b9f9a6000e19375858')
+.then(response => console.log("Success:", response))
+.catch(error => console.error("Error:", error));
 // Export the client instance
 export default client;
